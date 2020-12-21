a15b94d7b909c8e571753db04f8102434479bcb4
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-21 14:09:48 +0530

    dded firebase config

---
+++ stocks-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxbfiMazLDB1y0uH3bUMymYcZyIXvMwvQ",
+    authDomain: "stocks-app-81f3e.firebaseapp.com",
+    projectId: "stocks-app-81f3e",
+    storageBucket: "stocks-app-81f3e.appspot.com",
+    messagingSenderId: "481038876280",
+    appId: "1:481038876280:web:49daaedb188badc919b78d"
+  };
